112aea7c35820dfcf279b1f80eda14ad6bac073f
tanG <tanmay.gemini@gmail.com>
2024-10-09 15:31:32 +0530

    Update lead_app.py

--- lead_app.py
+++ lead_app.py
@@ -30,7 +30,7 @@ def wrap_labels(labels, max_length=25):
 # Load your dataframe
 @st.cache_data
 def load_data():
-    with open(Path(__file__).parent/'lead_app_data.pkl', 'rb') as file:
+    with open(Path(__file__).parent/'data/lead_app_data.pkl', 'rb') as file:
         loaded_dataframes = pickle.load(file)
     return loaded_dataframes
 
